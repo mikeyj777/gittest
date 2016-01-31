9e470ac5348ac4cd953a6cbc18f22f1c604f0f67
mikeyj777 <mikeyj777@gmail.com>
2016-01-30 23:52:56 -0500

    added addInts Function

--- gittest/ViewController.swift
+++ gittest/ViewController.swift
@@ -14,7 +14,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    func addInts(numA:Int, numB:Int) -> Int {
+        return numA + numB
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
